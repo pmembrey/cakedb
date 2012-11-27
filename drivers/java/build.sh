8d35a6edafbbf3ba45f313536f811ed5e09ac470
steve <stevecookhk@gmail.com>
2012-11-27 17:19:10 +0800

    dos2unix the build.sh script

--- drivers/java/build.sh
+++ drivers/java/build.sh
@@ -1,5 +1,5 @@
 #compile class files
-javac -cp /usr/share/java/junit4.jar org/cakedb/drivers/java/Event.java org/cakedb/drivers/java/Driver.java org/cakedb/drivers/java/UnitTestDriver.java
+javac -classpath /usr/share/java/junit4.jar org/cakedb/drivers/java/Event.java org/cakedb/drivers/java/Driver.java org/cakedb/drivers/java/UnitTestDriver.java
 #run unit test
 java -cp .:/usr/share/java/junit4.jar org.junit.runner.JUnitCore org.cakedb.drivers.java.UnitTestDriver
 #build jar

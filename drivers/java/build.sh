568600732890478b3f7c6a5b3cd6d2e0d99b666a
Peter Membrey <peter@membrey.hk>
2012-12-07 00:54:10 +0800

    Added junit dependencies into the repository.

--- drivers/java/build.sh
+++ drivers/java/build.sh
@@ -1,6 +1,6 @@
 #compile class files
-javac -classpath /usr/share/java/junit4.jar org/cakedb/drivers/java/Event.java org/cakedb/drivers/java/Driver.java org/cakedb/drivers/java/UnitTestDriver.java
+javac -classpath jars/junit-4.11.jar org/cakedb/drivers/java/Event.java org/cakedb/drivers/java/Driver.java org/cakedb/drivers/java/UnitTestDriver.java
 #run unit test
-java -cp .:/usr/share/java/junit4.jar org.junit.runner.JUnitCore org.cakedb.drivers.java.UnitTestDriver
+java -cp .:jars/junit-4.11.jar org.junit.runner.JUnitCore org.cakedb.drivers.java.UnitTestDriver
 #build jar
 jar cf cakedb001.jar org/cakedb/drivers/java/Event.class org/cakedb/drivers/java/Driver.class

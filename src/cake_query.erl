265099215fd2c6993652d3818b7af919c9508134
gary46 <gary46@gary46-VirtualBox.(none)>
2013-01-02 16:57:19 +0100

    Refactoring cake_query.erl

--- src/cake_query.erl
+++ src/cake_query.erl
@@ -7,9 +7,27 @@
          simple_query/3]).
 
 
-
 -define(MESSAGE_HEADER,<<1,1,1,TS:64/native-integer,Size:32/native-integer,CRC32:32/native-integer,2,2,2>>).
 -define(MESSAGE,<<Data:Size/binary,3,3,3>>).
+
+%% Thoughs on defining MESSAGE_PACKAGE as [?MESSAGE_HEADER, ?MESSAGE] or <<?MESSAGE_HEADER, ?MESSAGE>> for evolution.
+%% At this time, only TS, Size and Data are necessary for operation functions.
+%% CRC32 is processed at an upper level.
+-define(MESSAGE_PACKAGE, [TS:64/native-integer, Size:32/native-integer, Data:Size/binary]).
+
+
+
+retrieve_last_entry_at(StreamID,At) ->
+    retrieve_data(retrieve_last_entry_at, StreamID, [At])
+    .
+
+simple_query(StreamID,From,To) ->
+    retrieve_data(simple_query, StreamID, [From, To])
+    .
+
+all_since_query(StreamID,From) ->
+    retrieve_data(all_since_query, StreamID, [From])
+    .
 
 
 
@@ -18,111 +36,64 @@ open_data_file(FileName) ->
     {ok,DataFile} = file:open(DataDir ++ FileName ++ "/" ++ FileName ++ ".data",[binary,raw,read,read_ahead]),
     DataFile.
 
+retrieve_data(Operation, StreamID, ConditionList) ->
+    DataFile = retrieve_data_init(StreamID),
+    retrieve_data(Operation, DataFile, [], ConditionList)
+    .
 
-
-
-
-retrieve_last_entry_at(StreamID,At) ->
-    {ok,File} = cake_stream_manager:stream_filename(StreamID),
-    Offset = get_indexed_offset(StreamID,At),
+retrieve_data_init(StreamID) ->
+    {ok, File} = cake_stream_manager:stream_filename(StreamID),
+    Offset = get_indexed_offset(StreamID, At),
     lager:debug("Offset from get_indexed_offset: ~p", [Offset]),
     DataFile = open_data_file(File),
-    {ok,Offset} = file:position(DataFile,Offset),
-    retrieve_last_entry_at(DataFile,[],At).
+    {ok, Offset} = file:position(DataFile, Offset),
+    DataFile
+    .
 
-
-retrieve_last_entry_at(DataFile,FoundData,At) ->
-    case file:read(DataFile,22) of
-        {ok,?MESSAGE_HEADER} ->
-            case file:read(DataFile,Size+3) of
+%% Common recursive part for each query
+retrieve_data(Operation, DataFile, FoundData, ConditionList) ->
+    case file:read(DataFile, 22) of
+        {ok, ?MESSAGE_HEADER} ->
+            case file:read(DataFile, Size+3) of
                 {ok,?MESSAGE} -> case CRC32 == erlang:crc32(Data) of
-                                                true -> case TS < At of
-                                                            true -> retrieve_last_entry_at(DataFile,<<TS:64/big-integer,Size:32/big-integer,Data/binary>>,At);
-                                                            false -> retrieve_last_entry_at(DataFile,<<>>,At)
-                                                        end;
+                                                true -> retrieve_data(Operation, DataFile, FoundData, ConditionList, ?MESSAGE_PACKAGE)
                                                 false -> ok = file:close(DataFile),
-                                                         lager:warning("Message: CRC32 Checksum failed on ~p",[TS]),
+                                                         lager:warning("Message: CRC32 Checksum failed on ~p", [TS]),
                                                          FoundData
                                             end;
                 eof -> ok = file:close(DataFile),
-                       lager:warning("EOF: Not enough data - attempted to read ~p bytes",[Size]),
+                       lager:warning("EOF: Not enough data - attempted to read ~p bytes", [Size]),
                        FoundData
             end;
         _ -> ok = file:close(DataFile), 
             FoundData
-    end.
+    end
+    .
 
+retrieve_data(retrieve_last_entry_at, DataFile, _FoundData, [At], ?MESSAGE_PACKAGE) ->
+    case TS < At of
+	true -> retrieve_data(retrieve_last_entry_at, DataFile, <<TS:64/big-integer, Size:32/big-integer, Data/binary>>, At);
+        false -> retrieve_data(retrieve_last_entry_at, DataFile, <<>>, At)
+    end
+    .
 
+retrieve_data(simple_query, DataFile, FoundData, [From, To], ?MESSAGE_PACKAGE) ->
+    case (TS >= From) and (TS =< To) of
+	true -> retrieve_data(simple_query, DataFile, [<<TS:64/big-integer, Size:32/big-integer, Data/binary>> | FoundData], From, To);
+	false -> case TS < To of 
+		     true -> retrieve_data(simple_query, DataFile, FoundData, From, To);
+                     false -> ok = file:close(DataFile),
+                     list_to_binary(lists:flatten(lists:reverse(FoundData)))
+		 end
+    end
+    .
 
-simple_query(StreamID,From,To) ->
-    {ok,File} = cake_stream_manager:stream_filename(StreamID),
-    Offset = get_indexed_offset(StreamID,From),
-    lager:debug("Offset from get_indexed_offset: ~p", [Offset]),
-    DataFile = open_data_file(File),
-    {ok,Offset} = file:position(DataFile,Offset),
-    simple_query(DataFile,[],From,To).
-
-
-simple_query(DataFile,FoundData,From,To) ->
-    case file:read(DataFile,22) of
-        {ok,?MESSAGE_HEADER} ->
-            case file:read(DataFile,Size+3) of
-                {ok,?MESSAGE} -> case CRC32 == erlang:crc32(Data) of
-                                                true -> case (TS >= From) and (TS =< To) of
-                                                            true -> simple_query(DataFile,[<<TS:64/big-integer,Size:32/big-integer,Data/binary>>|FoundData],From,To);
-                                                            false -> case TS < To of 
-                                                                        true -> simple_query(DataFile,FoundData,From,To);
-                                                                        false -> ok = file:close(DataFile),
-                                                                                 list_to_binary(lists:flatten(lists:reverse(FoundData)))
-                                                                    end
-                                                        end;
-                                                false -> ok = file:close(DataFile),
-                                                         lager:warning("Message: CRC32 Checksum failed on ~p",[TS]),
-                                                         list_to_binary(lists:flatten(lists:reverse(FoundData)))
-                                            end;
-                eof -> ok = file:close(DataFile),
-                       lager:warning("EOF: Not enough data - attempted to read ~p bytes",[Size]),
-                       list_to_binary(lists:flatten(lists:reverse(FoundData)))
-            end;
-        _ -> ok = file:close(DataFile), 
-            list_to_binary(lists:flatten(lists:reverse(FoundData)))
-    end.
-
-
-
-
-
-
-
-all_since_query(StreamID,From) ->
-    {ok,File} = cake_stream_manager:stream_filename(StreamID),
-    Offset = get_indexed_offset(StreamID,From),
-    lager:debug("Offset from get_indexed_offset: ~p", [Offset]),
-    DataFile = open_data_file(File),
-    {ok,Offset} = file:position(DataFile,Offset),
-    all_since_query(DataFile,[],From).
-
-
-all_since_query(DataFile,FoundData,From) ->
-    case file:read(DataFile,22) of
-        {ok,?MESSAGE_HEADER} ->
-            case file:read(DataFile,Size+3) of
-                {ok,?MESSAGE} -> case CRC32 == erlang:crc32(Data) of
-                                                true -> case TS > From of
-                                                            true -> all_since_query(DataFile,[<<TS:64/big-integer,Size:32/big-integer,Data/binary>>|FoundData],From);
-                                                            false -> all_since_query(DataFile,FoundData,From)
-                                                        end;
-                                                false -> ok = file:close(DataFile),
-                                                         lager:warning("Message: CRC32 Checksum failed on ~p",[TS]),
-                                                         list_to_binary(lists:flatten(lists:reverse(FoundData)))
-                                            end;
-                eof -> ok = file:close(DataFile),
-                       lager:warning("EOF: Not enough data - attempted to read ~p bytes",[Size]),
-                       list_to_binary(lists:flatten(lists:reverse(FoundData)))
-            end;
-        _ -> ok = file:close(DataFile), 
-            list_to_binary(lists:flatten(lists:reverse(FoundData)))
-    end.
+retrieve_data(all_since_query, DataFile, FoundData, [From], ?MESSAGE_PACKAGE) ->
+    case TS > From of
+	true -> retrieve_data(all_since_query, DataFile, [<<TS:64/big-integer, Size:32/big-integer, Data/binary>> | FoundData], From);
+        false -> retrieve_data(all_since_query, DataFile, FoundData, From)
+    end
+    .
 
 
 
